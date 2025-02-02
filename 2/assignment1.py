ec0dcf038da1f8b53739eeabff053af9f0aa8179
andy33366 <andy704389@gmail.com>
2025-02-02 14:49:02 -0600

    started work on assignment 1

---
+++ 2/assignment1.py
@@ -0,0 +1,13 @@
+# this program will provide random addition and subtraction questions until the stutent quits
+
+correct = 0
+
+userInput = ""
+
+#large loop that check if student wants to quit
+while(userInput != "quit"):
+    #mathGame()
+
+def mathGame():
+
+
