40c01e375efb83bacf43bc02c725e6689ad2cf65
andy33366 <andy704389@gmail.com>
2025-02-26 10:19:48 -0600

    toy factory lab outline

---
+++ 4/Toy.py
@@ -0,0 +1,9 @@
+#Toy class
+
+def __init__(self, name):
+    self.name = name
+
+def getName():
+    return self.name
+
+
---
+++ 4/ToyFactory.py
@@ -0,0 +1,41 @@
+#ToyFactory class
+from Toy import Toy
+
+
+def __init__(self):
+    #100 heads
+    #100 bodies
+    #200 arms
+    #200 legs
+    #5 workers
+    #status message - blank
+
+def makeToy(self, name):
+    #grabParts() --> if false print message
+    #assemble() --> if false print message
+    # if (grabParts() && assemble()):
+    #   return Toy(name)
+    #else:
+    #   print message
+
+def grabParts(self):
+    #-1 head
+    #-1 body
+    #-2 arms
+    #-2 legs
+    #return OOSCheck()
+    
+
+def assemble(self):
+    #-1 worker
+    #return laborCheck()
+
+def laborCheck(self):
+    #if workers == 0 then set labor message && return false
+
+def OOSCheck(self):
+    #the factory is out of stock
+    #if head or body of arms or legs == 0, then set message OOS, return false
+
+
+
