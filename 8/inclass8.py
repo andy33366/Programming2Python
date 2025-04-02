0e5273dabea432f8e9af472dc3d8f0c5a5f00b9e
andy33366 <andy704389@gmail.com>
2025-04-02 14:12:27 -0500

    in class exercise 8

---
+++ 8/inclass8.py
@@ -0,0 +1,16 @@
+ 
+def main():
+
+    #sdjvakdjv
+    curriculum = {}
+    curriculum[2026] = "Software Engineering"
+    curriculum[2024] = "Programming Fundamentals II"
+    curriculum[2025] = "Data Structures"
+
+    print(curriculum)
+
+    curriculum = sorted(curriculum.items())
+
+    print(curriculum)
+
+main()
